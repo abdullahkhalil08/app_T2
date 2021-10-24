8ab7587582a61fde8cd5346abb17f1115068eb3b
khazaina <>
2021-10-24 20:14:11 +0500

    first

---
+++ app/src/main/java/com/example/abdullah/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.abdullah;
+
+import android.os.Bundle;
+import android.util.JsonReader;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
