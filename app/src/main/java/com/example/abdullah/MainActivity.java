6020500f41e9f7a2199dc866e6a7c4258c78463d
khazaina <>
2021-10-24 20:43:02 +0500

    second

--- app/src/main/java/com/example/abdullah/MainActivity.java
+++ app/src/main/java/com/example/abdullah/MainActivity.java
@@ -13,8 +13,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
+        String[] quest={"Linus Torvalds created Linux and Git",
+                "The programming language Python is based off a modified version of JavaScript",
+                "Pointers were not used in the original C programming language; they were added later on in C++.",
+                "RAM stands for Random Access Memory",
+                "HTML; stands for Hypertext Markup Language."};
+
+        boolean[] answers={true,false,false,true,true};
+
+
+        TextView question,win;
+        Button ok,wrong;
+        int score=0,i=0;
+        JSONArray quiz= null;
+        JSONObject temp=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
